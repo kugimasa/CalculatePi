4d1964443582ef4c933d0290a58e5e516634b03d
kugi <40158101+kugimasa@users.noreply.github.com>
2022-06-21 14:11:06 +0000

    Add Stratified Sampling

--- CalculatePi.cpp
+++ CalculatePi.cpp
@@ -70,17 +70,34 @@ int main (int argc, char *argv[])
     random_device seed_gen;
     mt19937 seed(seed_gen());
     int quater_points = 0;
+    int stratified_points = 0;
     for (int i = 0; i < num; i++)
     {
         constexpr size_t bits = numeric_limits<float>::digits;
+
+        // x, y [0,1)
         float x = generate_canonical<float, bits>(seed);
         float y = generate_canonical<float, bits>(seed);
         if (pointInQuater(x, y))
         {
             quater_points++;
         }
+
+        // Stratified Sampling
+        int sqrt_num = (int)sqrt(num);
+        int j = i / sqrt_num;
+        int k = i % sqrt_num;
+        x = x / (float)sqrt_num + j / (float)sqrt_num;
+        y = y / (float)sqrt_num + k / (float)sqrt_num;
+        if (pointInQuater(x, y))
+        {
+            stratified_points++;
+        }
     }
     float pi = calculatePi(quater_points, num);
     cout << "π : " << pi << endl;
+
+    pi = calculatePi(stratified_points, num);
+    cout << "π(stratified) : " << pi << endl;
     return 0;
 }
